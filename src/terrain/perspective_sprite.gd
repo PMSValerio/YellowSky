42ab3e71ce738412d4cb50847a2040bb1055cffc
PMSValerio <37459155+PMSValerio@users.noreply.github.com>
2022-10-13 21:49:54 +0100

    sprite scaling on perspective

--- src/terrain/perspective_sprite.gd
+++ src/terrain/perspective_sprite.gd
@@ -1,5 +1,5 @@
 extends Sprite
-
+class_name PerspectiveSprite
 
 export var HORIZON_Y = 0.7
 export var STRETCH_FACTOR = 0.4
@@ -11,49 +11,26 @@ var b = STRETCH_FACTOR
 var h = HORIZON_Y
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var tilemap = Global.get_ref_tilemap()
 	var screen_pos = tilemap.get_viewport_transform() * (tilemap.get_global_transform() * get_parent().global_position)
 	screen_pos.x /= tilemap.get_viewport_rect().size.x
 	screen_pos.y = 1.0 - (screen_pos.y / tilemap.get_viewport_rect().size.y)
 	
-	#print(screen_pos)
-	
-#	var p = (a * h) / (a - b)
-#
-#	var yy = screen_pos.y * a * h
-#	yy = yy / (b * h + screen_pos.y * (a - b))
-#
-#	var xx = (screen_pos.x / p) * (p - yy)
-#
-#	var d = (a * yy) / (2.0 * p)
-#
-#	screen_pos.x = d + xx
-#	screen_pos.y = yy
-	
-	#print(screen_pos)
-	#print("---")
-	
 	var bb = 1.0 / b
 	var q = (bb - a) / 2.0
-	#var w = screen_pos.x + q
 	
 	var p = (bb * h) / (bb - a)
 	
 	var yy = screen_pos.y * bb * h
 	yy = yy / (a * h + screen_pos.y * (bb - a))
 	
-	#var ww = (w * (p - yy)) / p
 	var w = (screen_pos.x * (p - yy)) / (p - h)
 	
 	var d = (q * (h - yy)) / h
 	
-	#screen_pos.x = ww - d
 	screen_pos.x = w - d
 	screen_pos.y = yy
-	
-	print(screen_pos)
-	#print("---")
 	
 	screen_pos.x *= tilemap.get_viewport_rect().size.x
 	screen_pos.y = (1.0 - screen_pos.y) * tilemap.get_viewport_rect().size.y
@@ -61,3 +38,10 @@ func _physics_process(delta: float) -> void:
 	var pos = tilemap.get_global_transform().affine_inverse() * (tilemap.get_viewport_transform().affine_inverse() * screen_pos)
 	
 	global_position = pos
+	var scl = (1.0 - yy) * 3.0
+	global_scale = Vector2(scl, scl)
+	
+	if yy <= h:
+		visible = true
+	else:
+		visible = false
