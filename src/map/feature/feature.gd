431d488db762dd298af1c248cd9f5dfe30e52f39
PMSValerio <37459155+PMSValerio@users.noreply.github.com>
2022-12-13 12:24:37 +0000

    tutorial indicator

--- src/map/feature/feature.gd
+++ src/map/feature/feature.gd
@@ -1,9 +1,16 @@
 extends Node2D
 class_name Feature
 
+
 func interact() -> void:
-	print("feature")
+	if has_node("Warning"):
+		$Warning.visible = false
 	EventManager.emit_signal("push_menu", Global.Menus.TEST, null)
+
+
+func _physics_process(_delta: float) -> void:
+	if has_node("Warning"):
+		$Warning.position = $Sprite.position + Vector2(0, -32) * $Sprite.scale
 
 
 func mouse_entered() -> void:
