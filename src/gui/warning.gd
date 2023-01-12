63a8a228116e0cf161013dc08b436b8a334822bb
PMSValerio <37459155+PMSValerio@users.noreply.github.com>
2023-01-12 03:08:26 +0000

    tweak to facility tooltip

--- src/gui/warning.gd
+++ src/gui/warning.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 
+onready var border_left = 32
+onready var border_right = MapUtils.screen_wid - 32
+onready var border_up = (1-MapUtils.HORIZON_Y) * MapUtils.screen_hei - 32
+onready var border_down = MapUtils.screen_hei - 96 - 32
+
 var rect = Rect2(0, 0, 40, 40)
 var mouse_in = false
 
 
 func _ready() -> void:
-	$MenuTooltip.set_text("[Click to Dismiss]")
+	set_tooltip_text("[Click to Dismiss]")
 
 
 func _input(event: InputEvent) -> void:
@@ -18,16 +23,16 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(_delta: float) -> void:
 	var tilemap = MapUtils.get_ref_tilemap()
-	rect.position = tilemap.get_viewport_transform() * (tilemap.get_global_transform() * global_position) - rect.size/2
-#	var mouse = Global.get_mouse_in_perspective()
+	var pos = tilemap.get_viewport_transform() * (tilemap.get_global_transform() * global_position)
+	pos.x = clamp(pos.x, border_left, border_right)
+	pos.y = clamp(pos.y, border_up, border_down)
+	$Anchor.global_position = tilemap.get_global_transform().affine_inverse() * (tilemap.get_viewport_transform().affine_inverse() * pos)
+	
+	rect.position = pos - rect.size/2
 	var mouse = get_viewport().get_mouse_position()
 	mouse_in = rect.has_point(mouse)
-	$MenuTooltip.visible = mouse_in
+	$Anchor/MenuTooltip.visible = mouse_in
 
 
-func _on_MenuTooltipProxy_mouse_entered() -> void:
-	mouse_in = true
-
-
-func _on_MenuTooltipProxy_mouse_exited() -> void:
-	mouse_in = false
+func set_tooltip_text(text : String):
+	$Anchor/MenuTooltip.set_text(text)
