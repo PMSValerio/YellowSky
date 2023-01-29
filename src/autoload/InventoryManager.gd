184eac7e61ea000e2b750d4161594c44eb9094d2
PMSValerio <37459155+PMSValerio@users.noreply.github.com>
2023-01-29 10:47:57 +0000

    generate food items from facility

--- src/autoload/InventoryManager.gd
+++ src/autoload/InventoryManager.gd
@@ -8,6 +8,9 @@ var inventory : Inventory = null
 var compact_resource_items = {} # ids of all compact resource items organised by value
 								# {<resource type>: [item_id] }
 
+var cannery_foods = []
+var hydro_foods = []
+
 
 # --- || Ready || ---
 
@@ -31,15 +34,15 @@ func _ready():
 # populate inventory data from config files
 func _build_items():
 	var config_file = "resources.json"
-	_build_item_category(config_file, true)
+	_build_item_category(config_file, Global.Items.RESOURCES)
 	config_file = "food.json"
-	_build_item_category(config_file)
+	_build_item_category(config_file, Global.Items.FOOD)
 	config_file = "quests.json"
 	_build_item_category(config_file)
 	
 
 # build a category of items
-func _build_item_category(file, resources = false):
+func _build_item_category(file, t = -1):
 
 	var file_data = Global.get_text_from_file(Global.Text.ITEMS, file, [])
 
@@ -47,8 +50,13 @@ func _build_item_category(file, resources = false):
 		var item : Item = _build_single_item(file_data[i])
 		item_stats[item.id] = item
 
-		if resources:
+		if t == Global.Items.RESOURCES:
 			compact_resource_items[item.subtype].append(item.id)
+		elif t == Global.Items.FOOD:
+			if item.subtype == Global.FacilityTypes.CANNERY:
+				cannery_foods.append(item.id)
+			elif item.subtype == Global.FacilityTypes.HYDROPONICS:
+				hydro_foods.append(item.id)
 
 
 # build a single item from data
@@ -61,6 +69,9 @@ func _build_single_item(data):
 
 	if type == Global.Items.RESOURCES:
 		item.subtype = Global.Resources[data["subtype"]]
+	elif type == Global.Items.FOOD:
+		if data["subtype"] in Global.FacilityTypes:
+			item.subtype = Global.FacilityTypes[data["subtype"]]
 
 	return item
 
@@ -70,3 +81,17 @@ func _build_single_item(data):
 
 func get_all_compact_ids(resource_type) -> Array:
 	return compact_resource_items[resource_type] if resource_type in compact_resource_items else []
+
+
+func add_food_amount(amount, fac_type) -> void:
+	var source = []
+	match fac_type:
+		Global.FacilityTypes.CANNERY:
+			source = cannery_foods
+		Global.FacilityTypes.HYDROPONICS:
+			source = hydro_foods
+	while amount > 0:
+		var ix = randi() % source.size()
+		var item = item_stats[source[ix]] as Item
+		amount -= item.stat
+		inventory.add_items(item.type, item.id, 1)
