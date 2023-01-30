3ed6e17a738043306438810959d367bb0098cd72
PMSValerio <37459155+PMSValerio@users.noreply.github.com>
2023-01-30 16:20:55 +0000

    end rain

--- src/disasters/rainfall/rainfall.gd
+++ src/disasters/rainfall/rainfall.gd
@@ -16,3 +16,7 @@ func set_rain_speed(speed : float, rain_ix : int):
 
 func map_rain(period : float):
 	EventManager.emit_signal("rain", period)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	emit_signal("disaster_end")
